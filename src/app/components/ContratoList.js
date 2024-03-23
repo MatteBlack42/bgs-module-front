82f01e23b255c05f17f004102e4cd97725d03958
Sergio Ortiz <owes423349@gmail.com>
2024-03-22 20:37:00 -0500

    Contratos

---
+++ src/app/components/ContratoList.js
@@ -0,0 +1,33 @@
+'use client';
+import styles from './Colaborador.module.css';
+import Link from 'next/link';
+import Image from 'next/image';
+
+export const ContratoList = ({ contratos }) => {
+    return (
+        <div className={styles.ColaboradorContainer}>
+            {contratos.data.map(contrato => (
+                <div key={contrato.id} className={styles.Colaborador}>
+                    <p>{contrato.id}</p>
+                    {contrato.attributes.EPS ? (
+                        <p>Si recibe</p>
+                    ) : (
+                        <p>No recibe</p>
+                    )}
+
+                    {contrato.attributes.seguro ? (
+                        <p>Si recibe</p>
+                    ) : (
+                        <p>No recibe</p>
+                    )}
+
+                    <p>{contrato.attributes.TiempoLibre}</p>
+
+                    <div className={styles.Buttons}>
+                        <button className={styles.Editar}>Asignar</button>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
--- src/app/contratos/page.js
+++ src/app/contratos/page.js
@@ -1,10 +1,71 @@
 import Image from 'next/image';
-// import styles from './page.module.css';
+import styles from '../page.module.css';
+import { revalidatePath, revalidateTag } from 'next/cache';
+import { ColaboradorList } from '../components/ColaboradorList';
+import { ContratoList } from '../components/ContratoList';
 
-export default function Contratos() {
+async function getContrato() {
+    const response = await fetch(
+        'https://dazzling-flame-563a188d67.strapiapp.com/api/contratoes',
+        {
+            cache: 'no-store',
+            method: 'GET',
+        }
+    );
+    const data = await response.json();
+    return data;
+    setRefresh(!refresh);
+}
+
+async function deleteColaborador(id) {
+    'use server';
+    const response = await fetch(
+        `https://dazzling-flame-563a188d67.strapiapp.com/api/colaboradors/${id}`,
+        {
+            method: 'DELETE',
+        }
+    );
+    const data = await response.json();
+    console.log(data);
+    revalidatePath('/');
+}
+async function cesarColaborador(id, state) {
+    'use server';
+    const cesarColaborador = {
+        data: {
+            Cesado: !state,
+        },
+    };
+    const response = await fetch(
+        `https://dazzling-flame-563a188d67.strapiapp.com/api/colaboradors/${id}`,
+        {
+            method: 'PUT',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(cesarColaborador),
+        }
+    );
+    const data = await response.json();
+    console.log(data);
+    revalidatePath('/');
+}
+
+export default async function Contratos() {
+    const contratos = await getContrato();
+
     return (
-        <main>
+        <main className={styles.main}>
             <h1>Contratos</h1>
+            <div className={styles.ColaboradorTitle}>
+                <h3>Número</h3>
+                <h3>EPS</h3>
+                <h3>Seguro</h3>
+                <h3>Días Libres</h3>
+                <h3>Acciones</h3>
+            </div>
+            <ContratoList contratos={contratos} />
         </main>
     );
 }
